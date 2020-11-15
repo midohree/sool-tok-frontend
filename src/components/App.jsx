f9f055b6de17e745273ee7fe70eb65ce626905f1
Ethan Shin <shinhc5@gmail.com>
2020-11-15 16:17:26 +0900

    :fire: remove test code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,6 @@ function App({ onLogin, onLoad, user }) {
     onLoad();
   }, []);
 
-  // TODO: 임시 처리 - 로그아웃 하고 새로고침 하지 않은 상태에서 로그인하면 마이페이지는 항상 꺼져 있게 유지 (확인 필요)
   useEffect(() => {
     if (!user) {
       setOpenMyPage(false);
